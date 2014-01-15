2dffa5779d8da81f3870121d9723516466520375
Zetter <zetter@gmail.com>
2014-01-15 22:05:45 +0000

    Only parse once for great speed up.
    BEFORE:
    Finished in 0.191506s, 26.1088 runs/s, 151.4313 assertions/s.
    
    AFTER:
    Finished in 0.004089s, 1222.7929 runs/s, 7092.1986 assertions/s.

--- lib/schemer/parser.rb
+++ lib/schemer/parser.rb
@@ -3,9 +3,10 @@ require 'treetop'
 class Schemer::Parser
   class ParseError < StandardError; end
 
+  PARSER = Treetop.load('schemer').new
+
   def self.parse(string)
-    parser = Treetop.load('schemer').new
-    tree = parser.parse(string)
+    tree = PARSER.parse(string)
     if tree.nil?
       raise ParseError, "Parse error at offset: #{parser.index}"
     end
