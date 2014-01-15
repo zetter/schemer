acb834d0028d9e330caff8f86513cc9eeeb5b295
Zetter <zetter@gmail.com>
2014-01-15 22:03:33 +0000

    Split up tests

--- test/01_toys_test.rb
+++ test/01_toys_test.rb
@@ -14,15 +14,22 @@ describe '1. Toys' do
   it 'parses lists and expressions' do
     assert_parses_to_kind_of(Schemer::List, '(atom)')
     assert_parses_to_kind_of(Schemer::List, '(atom turkey or)')
+  end
 
+  it 'parses multiple expressions' do
     ast = ast_for('(atom turkey) or')
     assert_equal 2, ast.children.length
     assert_kind_of(Schemer::Expression, ast.children.first)
     assert_kind_of(Schemer::List, ast.children.first)
     assert_kind_of(Schemer::Expression, ast.children.last)
     assert_kind_of(Schemer::Atom, ast.children.last)
+  end
 
+  it 'parses nested lists' do
     assert_parses_to_kind_of(Schemer::List, '((atom turkey) or)')
+  end
+
+  specify 'lists and atoms are expressions' do
     assert_parses_to_kind_of(Schemer::Expression, 'xyz')
     assert_parses_to_kind_of(Schemer::Expression, '(x y z)')
     assert_parses_to_kind_of(Schemer::Expression, '((x y) z)')
