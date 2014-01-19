e9b46b1b43ddfa383720250e82aec20d5413977a
Zetter <zetter@gmail.com>
2014-01-19 16:52:31 +0000

    Extract functions to separate class

---
+++ lib/schemer/function.rb
@@ -0,0 +1,66 @@
+module Schemer
+  class Function
+    attr_reader :expressions
+
+    def initialize(expressions)
+      @expressions = expressions
+    end
+
+    def execute
+      case function
+      when nil
+        List.new
+      when Atom.new('car')
+        raise RuntimeError.new('arg for car must be a non-empty list') if !arg.list? || arg.empty?
+        arg.children[0]
+      when Atom.new('cdr')
+        raise RuntimeError.new('arg for cdr must be a non-empty list') if !arg.list? || arg.empty?
+        List.new(*arg.children.drop(1))
+      when Atom.new('cons')
+        raise RuntimeError.new('second arg for cons must be a list') unless arg_2.list?
+        List.new(*arg_2.children.unshift(arg_1))
+      when Atom.new('null?')
+        raise RuntimeError.new('arg for cdr must be a list') unless arg.list?
+        to_true_or_false(arg == List.new)
+      when Atom.new('quote')
+        List.new
+      when Atom.new('atom?')
+         to_true_or_false(arg.atom?)
+      when Atom.new('eq?')
+        unless arg_1.atom? && arg_2.atom? && arg_1.non_numeric? && arg_2.non_numeric?
+          raise RuntimeError.new('args for eql must non-numeric atoms')
+        end
+        to_true_or_false(arg_1 == arg_2)
+      else
+        List.new(*expressions)
+      end
+    end
+
+    private
+
+    def arguments
+      expressions.drop(1)
+    end
+
+    def arg_1
+      arguments[0]
+    end
+    alias :arg :arg_1
+
+    def arg_2
+      arguments[1]
+    end
+
+    def function
+      expressions[0]
+    end
+
+    def to_true_or_false(boolean)
+      if boolean
+        Atom.new('#t')
+      else
+        Atom.new('#f')
+      end
+    end
+  end
+end
--- lib/schemer/list.rb
+++ lib/schemer/list.rb
@@ -2,60 +2,11 @@ module Schemer
   class List < Expression
     def run
       @children = children.map(&:run)
-
-      if function == Atom.new('car')
-        raise RuntimeError.new('arg for car must be a non-empty list') if !arg.list? || arg.empty?
-        arg.children[0]
-      elsif function == Atom.new('cdr')
-        raise RuntimeError.new('arg for cdr must be a non-empty list') if !arg.list? || arg.empty?
-        List.new(*arg.children.drop(1))
-      elsif function == Atom.new('cons')
-        raise RuntimeError.new('second arg for cons must be a list') unless arg_2.list?
-        List.new(*arg_2.children.unshift(arg_1))
-      elsif function == Atom.new('null?')
-        raise RuntimeError.new('arg for cdr must be a list') unless arg.list?
-        to_true_or_false(arg == List.new)
-      elsif function == Atom.new('quote')
-        List.new
-      elsif function == Atom.new('atom?')
-         to_true_or_false(arg.atom?)
-      elsif function == Atom.new('eq?')
-        unless arg_1.atom? && arg_2.atom? && arg_1.non_numeric? && arg_2.non_numeric?
-          raise RuntimeError.new('args for eql must non-numeric atoms')
-        end
-        to_true_or_false(arg_1 == arg_2)
-      else
-        self
-      end
+      Function.new(children).execute
     end
 
     def list?
       true
-    end
-
-    def arguments
-      children.drop(1)
-    end
-
-    def arg_1
-      arguments[0]
-    end
-    alias :arg :arg_1
-
-    def arg_2
-      arguments[1]
-    end
-
-    def function
-      children[0]
-    end
-
-    def to_true_or_false(boolean)
-      if boolean
-        Atom.new('#t')
-      else
-        Atom.new('#f')
-      end
     end
 
     def empty?
