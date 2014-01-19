35bb08cdd91c7f971bf223a9fce3208f29543796
Zetter <zetter@gmail.com>
2014-01-19 15:57:18 +0000

    helpers for getting arguments

--- lib/schemer/list.rb
+++ lib/schemer/list.rb
@@ -4,22 +4,17 @@ module Schemer
       @children = children.map(&:run)
 
       if function == Atom.new('car')
-        arg = arguments.first
         raise RuntimeError.new('arg for car must be a list') unless arg.list?
         raise RuntimeError.new('arg for car cannot be an empty list') if arg.empty?
         arg.children[0]
       elsif function == Atom.new('cdr')
-        arg = arguments.first
         raise RuntimeError.new('arg for cdr must be a list') unless arg.list?
         raise RuntimeError.new('arg for cdr cannot be an empty list') if arg.empty?
         List.new(*arg.children.drop(1))
       elsif function == Atom.new('cons')
-        arg_1 = arguments[0]
-        arg_2 = arguments[1]
         raise RuntimeError.new('second arg for cons must be a list') unless arg_2.list?
         List.new(*arg_2.children.unshift(arg_1))
       elsif function == Atom.new('null?')
-        arg = arguments.first
         raise RuntimeError.new('arg for cdr must be a list') unless arg.list?
         if arg == List.new
           Atom.new('#t')
@@ -29,15 +24,12 @@ module Schemer
       elsif function == Atom.new('quote')
         List.new
       elsif function == Atom.new('atom?')
-        arg = arguments.first
         if arg.atom?
           Atom.new('#t')
         else
           Atom.new('#f')
         end
       elsif function == Atom.new('eq?')
-        arg_1 = arguments[0]
-        arg_2 = arguments[1]
         unless arg_1.atom? && arg_2.atom?
           raise RuntimeError.new('args for eql must be atoms')
         end
@@ -60,6 +52,15 @@ module Schemer
 
     def arguments
       children.drop(1)
+    end
+
+    def arg_1
+      arguments[0]
+    end
+    alias :arg :arg_1
+
+    def arg_2
+      arguments[1]
     end
 
     def function
